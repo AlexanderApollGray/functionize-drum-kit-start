0ed0a0b756a4e2e31c05168c517593dae499f0ac
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-02-06 10:53:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,60 +8,31 @@ function processKeyDown(e) {
   console.log(e.code);
 
   if (e.code === 'KeyA') {
-    // Play A-Clap
-    let audio = document.getElementById('clap');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('A').classList.add('playing');
+    setKey('A', 'clap',)
   } else if (e.code === 'KeyS') {
-    // Play S-hihat
-    let audio = document.getElementById('hihat');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('S').classList.add('playing');
+    setKey('S', 'hihat',)
   } else if (e.code === 'KeyD') {
-    // Play D-kick
-    let audio = document.getElementById('kick');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('D').classList.add('playing');
+    setKey('D', 'kick',)
   } else if (e.code === 'KeyF') {
-    // Play F-Openhat
-    let audio = document.getElementById('openhat');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('F').classList.add('playing');
+    setKey('F', 'openhat',)
   } else if (e.code === 'KeyG') {
-    // Play G-Boom
-    let audio = document.getElementById('boom');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('G').classList.add('playing');
+    setKey('G', 'boom',)
   } else if (e.code === 'KeyH') {
-    // Play H-Ride
-    let audio = document.getElementById('ride');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('H').classList.add('playing');
+    setKey('H', 'ride',)
   } else if (e.code === 'KeyJ') {
-    // Play J-Snare
-    let audio = document.getElementById('snare');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('J').classList.add('playing');
+    setKey('J', 'snare',)
   } else if (e.code === 'KeyK') {
-    // Play K-Tom
-    let audio = document.getElementById('tom');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('K').classList.add('playing');
+    setKey('K', 'tom',)
   } else if (e.code === 'KeyL') {
-    // Play L-Tink
-    let audio = document.getElementById('tink');
-    audio.currentTime = 0;
-    audio.play();
-    document.getElementById('L').classList.add('playing');
+    setKey('L', 'tink',)
   }
+}
+
+function setKey(keyName, audioName) {
+  let audio = document.getElementById(audioName)
+  audio.currentTime = 0;
+  audio.play();
+  document.getElementById(keyName).classList.add('playing')
 }
 
 // Add transition end listeners
